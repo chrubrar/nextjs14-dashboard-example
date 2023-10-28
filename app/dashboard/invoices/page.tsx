0d2b4913748d8d091b26eceb51d65b36f5c511c4
Chru Brar <chrubrar@gmail.com>
2023-10-28 16:28:34 -0400

    first commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+
+
+
+export default function InvoicePage() {
+    return <p>Invoices Page</p>;
+
+};
---
+++ app/ui/fonts.ts
@@ -0,0 +1,19 @@
+import { Inter } from 'next/font/google';
+
+import { Lusitana } from 'next/font/google';
+
+import { Roboto } from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+
+export const lustiana = Lusitana({
+    weight: ['400','700'],
+    subsets:['latin']
+});
+
+export const roboto = Roboto({
+    weight: ['100','300', '400', '500', '700', '900'],
+    subsets:['latin']
+
+});
+
